90ba2c8b3239a43f38dad9b0858a7330d4b79be2
Justin Schnurer <JSchnurer@slb.com>
2024-11-13 16:23:34 -0500

    added cattail and swamp tree events

---
+++ src/scenes/objects/cattails/cattails.gd
@@ -0,0 +1,7 @@
+extends BaseEvent
+
+func investigate():
+	Utility.msg("Swamp.Shared.Cattail_Look")
+
+func take():
+	Utility.msg("Swamp.Shared.Cattail_Take")
---
+++ src/scenes/objects/tree/swamp_tree.gd
@@ -0,0 +1,7 @@
+extends BaseEvent
+
+func investigate():
+	Utility.msg("Swamp.Shared.Tree_Look")
+
+func take():
+	Utility.msg("Swamp.Shared.Tree_Take")
